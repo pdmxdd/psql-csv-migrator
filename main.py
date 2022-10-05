be272ebd153484671fc204f572da85d2950ab4a3
pdmxdd <pdmxdd@gmail.com>
2022-10-05 15:02:42 -0500

    f string for PSQL connection string

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ from queries.selectors import select_all_rows
 from transformers.row import to_dict
 from writers.table_to_csv import initialize_csv_for_table, write_row_dict_to_csv
 
+USERNAME = "postgres"
+PASSWORD = "postgres"
+HOST = "127.0.0.1"
+PORT = "5555"
+DATABASE = "postgres"
+
 def main():
-    engine = create_engine('postgresql://postgres:postgres@127.0.0.1:5555/postgres')
+    engine = create_engine(f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
     session = Session(engine)
 
     tables = [Users, Cars, CarsUsers]
